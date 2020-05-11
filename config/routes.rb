50c33b8f5c6192beea73e0710442e227fa5ecdf7
lokesh <lksh2812@gmail.com>
2020-05-11 09:46:10 +0530

    added view for stock search results

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
